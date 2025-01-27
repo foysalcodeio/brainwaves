4181c93143dfd0d53e3526befeb846ad1b3dc207
Md.Foysal Ahammed <md.foysalahammedforhad@gmail.com>
2025-01-28 00:54:16 +0600

    quater add

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,12 +1,18 @@
 import { useLocation } from 'react-router-dom';
 import { brainwave } from '../assets';
 import { navigation } from '../constants';
+import Button from './Button';
+import MenuSvg from '../assets/svg/MenuSvg'
+import {HamburgerMenu } from './design/Header'
+import { useState } from 'react';
+
 
 const Header = () => {
   const pathname = useLocation();
+  const [openNavigation, setOpenNavigation] = useState(false);
 
   return (
-    <div className='bg-n-8/90 fixed backdrop-blur-sm border-b border-n-6 lg:backdrop-blur-sm lg:border-n-6 top-0 z-50 '>
+    <div className='bg-n-8/90 fixed backdrop-blur-sm border-b border-n-6 lg:backdrop-blur-sm lg:border-n-6 top-0 left-0 z-50 w-full'>
       <div className='flex items-center px-5 lg:px-7.5 xl:px-10 max-lg:py-4'>
         <a className='block xl:mr-8 w-[12rem]' href="#hero">
           <img src={brainwave} width={190} height={40} alt="brainwave" />
@@ -20,9 +26,16 @@ const Header = () => {
                 {item.title} 
               </a>
             ))}
-
           </div>
         </nav>
+
+        <a href="#signup" className='button hidden mr-8 text-n-1/50 transition-colors hover:text-n-1 lg:block'>
+            New Account
+        </a>
+
+        <Button className="hidden lg:flex" href="#login">
+            Sign In
+        </Button>
 
       </div>
     </div>
