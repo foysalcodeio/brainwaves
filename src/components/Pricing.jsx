a9e2ac04abe494e825f4be57799c5489645875d1
Md.Foysal Ahammed <md.foysalahammedforhad@gmail.com>
2025-02-11 22:15:47 +0600

    working on chart section

--- src/components/Pricing.jsx
+++ src/components/Pricing.jsx
@@ -1,51 +1,34 @@
-import Section from "./Section";
 import { smallSphere, stars } from "../assets";
+import Section from "./design/Section";
 import Heading from "./Heading";
-import PricingList from "./PricingList";
-import { LeftLine, RightLine } from "./design/Pricing";
+
 
 const Pricing = () => {
   return (
     <Section className="overflow-hidden" id="pricing">
       <div className="container relative z-2">
-        <div className="hidden relative justify-center mb-[6.5rem] lg:flex">
-          <img
-            src={smallSphere}
-            className="relative z-1"
-            width={255}
-            height={255}
-            alt="Sphere"
-          />
+        <div className="lg:flex justify-center mb-[6.5rem] hidden relative">
+          <img src={smallSphere} className="relative z-1" height={255} width={255} alt="Sphere" />
+
           <div className="absolute top-1/2 left-1/2 w-[60rem] -translate-x-1/2 -translate-y-1/2 pointer-events-none">
             <img
               src={stars}
               className="w-full"
               width={950}
               height={400}
-              alt="Stars"
-            />
+              alt="Stars" />
           </div>
         </div>
-
         <Heading
-          tag="Get started with Brainwave"
-          title="Pay once, use forever"
+          tag="get started with Nuerowave"
+          title="Pay once, use anytime"
         />
 
-        <div className="relative">
-          <PricingList />
-          <LeftLine />
-          <RightLine />
+        <div className="">
+            <h1>hello</h1>
         </div>
 
-        <div className="flex justify-center mt-10">
-          <a
-            className="text-xs font-code font-bold tracking-wider uppercase border-b"
-            href="/pricing"
-          >
-            See the full details
-          </a>
-        </div>
+
       </div>
     </Section>
   );
